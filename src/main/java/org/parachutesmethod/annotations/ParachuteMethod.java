0934c9a350dd2a30d7955bde50bf456b5e08a8d1
Vladimir Yussupov <v.yussupov@gmail.com>
2019-03-01 13:22:51 +0100

    Set default values
    Signed-off-by: Vladimir Yussupov <v.yussupov@gmail.com>

--- src/main/java/org/parachutesmethod/annotations/ParachuteMethod.java
+++ src/main/java/org/parachutesmethod/annotations/ParachuteMethod.java
@@ -9,29 +9,27 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 public @interface ParachuteMethod {
     /**
-     * Retain other annotations in extracted functionality.
-     * False by default.
+     * Retain other annotations in extracted functionality. False by default.
      */
     boolean retainAnnotations() default false;
 
     /**
-     * Retain parachute annotations in extracted functionality.
-     * False by default.
+     * Retain parachute annotations in extracted functionality. False by default.
      */
     boolean retainParachuteAnnotations() default false;
 
     /**
      * Configure the method to be a backup route.
      */
-    boolean backupRoute();
+    boolean backupRoute() default false;
 
     /**
      * Specification of overprovisioning factor for router configuration.
      */
-    double overProvisioningFactor();
+    String overProvisioningFactor() default "[unassigned]";
 
     /**
      * Example condition for rerouting on specific day.
      */
-    String rerouteOnDay();
+    String rerouteOnDay() default "[unassigned]";
 }
